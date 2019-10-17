72ea614aeed0153886db3dfed4008009a1800f38
eddiechapman <eddie.chapman4@gmail.com>
2019-10-17 13:48:56 -0500

    Style formatting.

--- db/db.py
+++ db/db.py
@@ -25,13 +25,28 @@ Base = declarative_base()
 
 
 class Directorate(UniqueMixin, Base):
-    id = Column(Integer, primary_key=True)
-    name = Column(String(80), nullable=False)
-    code = Column(CHAR(4), unique=True)
-    phone = Column(String(15), unique=True)
+    id = Column(
+        Integer,
+        primary_key=True,
+    )
+    name = Column(
+        String(80),
+        nullable=False,
+    )
+    code = Column(
+        CHAR(4),
+        unique=True,
+    )
+    phone = Column(
+        String(15),
+        unique=True,
+    )
     divisions = saorm.relationship(
-        'Division', backref='directorate',
-        cascade='all, delete-orphan', passive_deletes=True)
+        'Division',
+        backref='directorate',
+        cascade='all, delete-orphan',
+        passive_deletes=True,
+    )
 
     def __init__(self, name, code=None, phone=None):
         self.name = name
@@ -48,16 +63,33 @@ class Directorate(UniqueMixin, Base):
 
 
 class Division(UniqueMixin, Base):
-    id = Column(Integer, primary_key=True)
-    name = Column(String(80), nullable=False)
-    code = Column(CHAR(4), unique=True)
-    phone = Column(String(15), unique=True)
+    id = Column(
+        Integer,
+        primary_key=True,
+    )
+    name = Column(
+        String(80),
+        nullable=False
+    )
+    code = Column(
+        CHAR(4),
+        unique=True,
+    )
+    phone = Column(
+        String(15),
+        unique=True,
+    )
     dir_id = Column(
-        Integer, ForeignKey('directorate.id', ondelete='CASCADE'),
-        nullable=False)
+        Integer,
+        ForeignKey('directorate.id', ondelete='CASCADE'),
+        nullable=False,
+    )
     programs = saorm.relationship(
-        'Program', backref='division',
-        cascade='all, delete-orphan', passive_deletes=True)
+        'Program',
+        backref='division',
+        cascade='all, delete-orphan',
+        passive_deletes=True,
+    )
 
     def __init__(self, name, code=None, phone=None, dir_id=None):
         self.name = name
@@ -75,15 +107,27 @@ class Division(UniqueMixin, Base):
 
 
 class Program(UniqueMixin, Base):
-    id = Column(Integer, primary_key=True)
-    code = Column(CHAR(4), unique=True, nullable=False)
+    id = Column(
+        Integer,
+        primary_key=True,
+    )
+    code = Column(
+        CHAR(4),
+        unique=True,
+        nullable=False,
+    )
     name = Column(String(80))
-    div_id = Column(CHAR(4), ForeignKey('division.id', ondelete='CASCADE'))
+    div_id = Column(
+        CHAR(4),
+        ForeignKey('division.id', ondelete='CASCADE'),
+    )
 
     related_programs = association_proxy(
-        '_related_programs', 'secondary',
+        '_related_programs',
+        'secondary',
         creator=lambda code, name: RelatedPrograms(
-            secondary=Program(code, name))
+            secondary=Program(code, name)
+        ),
     )
 
     def __init__(self, code, name=None, div_id=None):
@@ -102,24 +146,35 @@ class Program(UniqueMixin, Base):
 
 class RelatedPrograms(UniqueMixin, Base):
     pgm1_id = Column(
-        Integer, ForeignKey('program.id', ondelete='CASCADE'),
-        primary_key=True)
+        Integer,
+        ForeignKey('program.id', ondelete='CASCADE'),
+        primary_key=True,
+    )
     pgm2_id = Column(
-        Integer, ForeignKey('program.id', ondelete='CASCADE'),
-        primary_key=True)
+        Integer,
+        ForeignKey('program.id', ondelete='CASCADE'),
+        primary_key=True,
+    )
 
     primary = saorm.relationship(
-        'Program', foreign_keys='RelatedPrograms.pgm1_id',
-        uselist=False, single_parent=True,
+        'Program',
+        foreign_keys='RelatedPrograms.pgm1_id',
+        uselist=False,
+        single_parent=True,
         backref=saorm.backref(
-            '_related_programs', cascade='all, delete-orphan',
+            '_related_programs',
+            cascade='all, delete-orphan',
             collection_class=attribute_mapped_collection('secondary'),
-            passive_deletes=True)
+            passive_deletes=True,
+        ),
     )
 
     secondary = saorm.relationship(
-        'Program', foreign_keys='RelatedPrograms.pgm2_id',
-        uselist=False, single_parent=True)
+        'Program',
+        foreign_keys='RelatedPrograms.pgm2_id',
+        uselist=False,
+        single_parent=True,
+    )
 
     __table_args__ = (
         CheckConstraint(pgm1_id != pgm2_id),
@@ -137,12 +192,20 @@ class RelatedPrograms(UniqueMixin, Base):
     def unique_filter(cls, query, pgm1_id, pgm2_id):
         return query.filter(
             RelatedPrograms.pgm1_id == pgm1_id and
-            RelatedPrograms.pgm2_id == pgm2_id)
+            RelatedPrograms.pgm2_id == pgm2_id
+        )
 
 
 class Award(UniqueMixin, Base):
-    id = Column(Integer, primary_key=True)
-    code = Column(CHAR(7), nullable=False, unique=True)
+    id = Column(
+        Integer,
+        primary_key=True,
+    )
+    code = Column(
+        CHAR(7),
+        nullable=False,
+        unique=True,
+    )
     title = Column(String(100))
     abstract = Column(Text)
     effective = Column(Date)
@@ -154,11 +217,15 @@ class Award(UniqueMixin, Base):
     instrument = Column(String(100))
 
     publications = saorm.relationship(
-        'Publication', backref=saorm.backref('award', uselist=False))
+        'Publication',
+        backref=saorm.backref('award', uselist=False),
+    )
     institutions = association_proxy('affiliations', 'institution')
     people = association_proxy(
-        'affiliations', 'person',
-        creator=lambda kwargs: Person.from_fullname(**kwargs))
+        'affiliations',
+        'person',
+        creator=lambda kwargs: Person.from_fullname(**kwargs),
+    )
 
     @classmethod
     def unique_hash(cls, code, *args, **kwargs):
@@ -171,18 +238,29 @@ class Award(UniqueMixin, Base):
 
 class Funding(UniqueMixin, Base):
     pgm_id = Column(
-        Integer, ForeignKey('program.id', ondelete='CASCADE'),
-        primary_key=True)
+        Integer,
+        ForeignKey('program.id', ondelete='CASCADE'),
+        primary_key=True,
+    )
     award_id = Column(
-        Integer, ForeignKey('award.id', ondelete='CASCADE'),
-        primary_key=True)
+        Integer,
+        ForeignKey('award.id', ondelete='CASCADE'),
+        primary_key=True,
+    )
 
-    program = saorm.relationship('Program', uselist=False, single_parent=True)
+    program = saorm.relationship(
+        'Program',
+        uselist=False,
+        single_parent=True,
+    )
     award = saorm.relationship(
-        'Award', uselist=False, # single_parent=True,
+        'Award',
+        uselist=False,  # single_parent=True,
         backref=saorm.backref(
-            'funding_programs', cascade='all, delete-orphan',
-            passive_deletes=True)
+            'funding_programs',
+            cascade='all, delete-orphan',
+            passive_deletes=True,
+        ),
     )
 
     def __init__(self, program, award):
@@ -195,45 +273,95 @@ class Funding(UniqueMixin, Base):
 
     @classmethod
     def unique_filter(cls, query, pgm, award):
-        return query.filter(Funding.pgm_id == pgm.id and
-                            Funding.award_id == award.id)
+        return query.filter(
+            Funding.pgm_id == pgm.id and
+            Funding.award_id == award.id
+        )
 
 
 class Publication(BasicMixin, Base):
-    id = Column(Integer, primary_key=True)
-    title = Column(String(255), nullable=False)
+    id = Column(
+        Integer,
+        primary_key=True,
+    )
+    title = Column(
+        String(255),
+        nullable=False,
+    )
     abstract = Column(Text)
     journal = Column(String(255))
     volume = Column(String(10))
     pages = Column(String(30))
     year = Column(Integer)
     uri = Column(String(255))
-    award_id = Column(Integer, ForeignKey('award.id', ondelete='SET NULL'))
+    award_id = Column(
+        Integer,
+        ForeignKey('award.id', ondelete='SET NULL'),
+    )
 
 
 class State(BasicMixin, Base):
-    abbr = Column(CHAR(2), primary_key=True)
-    name = Column(String(14), nullable=False, unique=True)
+    abbr = Column(
+        CHAR(2),
+        primary_key=True,
+    )
+    name = Column(
+        String(14),
+        nullable=False,
+        unique=True,
+    )
 
 
 class Country(BasicMixin, Base):
-    alpha2 = Column(CHAR(2), primary_key=True)
-    name = Column(String(100), nullable=False)
+    alpha2 = Column(
+        CHAR(2),
+        primary_key=True,
+    )
+    name = Column(
+        String(100),
+        nullable=False,
+    )
 
 
 class Address(UniqueMixin, Base):
-    id = Column(Integer, primary_key=True)
-    street = Column(String(50), nullable=False)
-    city = Column(String(50), nullable=False)
-    state = Column(CHAR(2), ForeignKey('state.abbr'), nullable=False)
-    country = Column(CHAR(2), ForeignKey('country.alpha2'), nullable=False)
-    zipcode = Column(String(10), nullable=False)
+    id = Column(
+        Integer,
+        primary_key=True,
+    )
+    street = Column(
+        String(50),
+        nullable=False,
+    )
+    city = Column(
+        String(50),
+        nullable=False,
+    )
+    state = Column(
+        CHAR(2),
+        ForeignKey('state.abbr'),
+        nullable=False,
+    )
+    country = Column(
+        CHAR(2),
+        ForeignKey('country.alpha2'),
+        nullable=False,
+    )
+    zipcode = Column(
+        String(10),
+        nullable=False,
+    )
     lat = Column(FLOAT)
     lon = Column(FLOAT)
 
     __table_args__ = (
-        UniqueConstraint('street', 'city', 'state', 'country', 'zipcode',
-                         name='_address_uc'),
+        UniqueConstraint(
+            'street',
+            'city',
+            'state',
+            'country',
+            'zipcode',
+            name='_address_uc',
+        ),
     )
 
     @classmethod
@@ -247,11 +375,26 @@ class Address(UniqueMixin, Base):
 
 
 class Institution(UniqueMixin, Base):
-    id = Column(Integer, primary_key=True)
-    name = Column(String(100), nullable=False)
-    phone = Column(String(15), unique=True)
-    address_id = Column(Integer, ForeignKey('address.id', ondelete='SET NULL'))
-    address = saorm.relationship('Address', uselist=False)
+    id = Column(
+        Integer,
+        primary_key=True,
+    )
+    name = Column(
+        String(100),
+        nullable=False,
+    )
+    phone = Column(
+        String(15),
+        unique=True,
+    )
+    address_id = Column(
+        Integer,
+        ForeignKey('address.id', ondelete='SET NULL'),
+    )
+    address = saorm.relationship(
+        'Address',
+        uselist=False,
+    )
 
     people = association_proxy('_people', 'person')
 
@@ -265,21 +408,38 @@ class Institution(UniqueMixin, Base):
 
 
 class Person(UniqueMixin, Base):
-    id = Column(Integer, primary_key=True)
-    fname = Column(String(50), nullable=False)
-    lname = Column(String(50), nullable=False)
+    id = Column(
+        Integer,
+        primary_key=True,
+    )
+    fname = Column(
+        String(50),
+        nullable=False,
+    )
+    lname = Column(
+        String(50),
+        nullable=False,
+    )
     mname = Column(String(50))
     nickname = Column(String(20))
     title = Column(String(10))
     suffix = Column(String(10))
-    email = Column(String(100), unique=True)
+    email = Column(
+        String(100),
+        unique=True,
+    )
 
     publications = association_proxy('_publications', 'publication')
     institutions = association_proxy('affiliations', 'institution')
     awards = association_proxy('roles', 'award')
 
     __table_args__ = (
-        UniqueConstraint('fname', 'lname', 'mname', name='_person_name_uc'),
+        UniqueConstraint(
+            'fname',
+            'lname',
+            'mname',
+            name='_person_name_uc',
+        ),
     )
 
     @classmethod
@@ -293,14 +453,15 @@ class Person(UniqueMixin, Base):
     @classmethod
     def from_fullname(cls, session, name, email=None):
         parsed_name = nameparser.HumanName(name)
-        return cls.as_unique(session,
+        return cls.as_unique(
+            session,
             fname=parsed_name.first.strip('.'),
             lname=parsed_name.last.strip('.'),
             mname=parsed_name.middle.strip('.'),
             title=parsed_name.title.strip('.'),
             suffix=parsed_name.suffix.strip('.'),
             nickname=parsed_name.nickname.strip('.'),
-            email=email
+            email=email,
         )
 
     @hybrid_property
@@ -310,7 +471,7 @@ class Person(UniqueMixin, Base):
             pieces.append(self.title)
         pieces.append(self.fname)
         if self.nickname is not None:
-            pieces.append('({})'.format(self.nickname))
+            pieces.append(f'({self.nickname})')
         if self.mname is not None:
             pieces.append(self.mname)
         pieces.append(self.lname)
@@ -321,20 +482,32 @@ class Person(UniqueMixin, Base):
 
 class Author(UniqueMixin, Base):
     person_id = Column(
-        Integer, ForeignKey('person.id', ondelete='CASCADE'),
-        primary_key=True)
+        Integer,
+        ForeignKey('person.id', ondelete='CASCADE'),
+        primary_key=True,
+    )
     pub_id = Column(
-        Integer, ForeignKey('publication.id', ondelete='CASCADE'),
-        primary_key=True)
+        Integer,
+        ForeignKey('publication.id', ondelete='CASCADE'),
+        primary_key=True,
+    )
 
     person = saorm.relationship(
-        'Person', uselist=False, single_parent=True,
+        'Person',
+        uselist=False,
+        single_parent=True,
         backref=saorm.backref(
-            '_publications', cascade='all,delete-orphan', passive_deletes=True)
+            '_publications',
+            cascade='all,delete-orphan',
+            passive_deletes=True,
+        ),
     )
 
     publication = saorm.relationship(
-        'Publication', uselist=False, single_parent=True)
+        'Publication',
+        uselist=False,
+        single_parent=True,
+    )
 
     def __init__(self, person, pub):
         self.person_id = person.id
@@ -346,26 +519,41 @@ class Author(UniqueMixin, Base):
 
     @classmethod
     def unique_filter(cls, query, person_id, award_id, *args, **kwargs):
-        return query.filter(Role.person_id == person_id and
-                            Role.award_id == award_id)
+        return query.filter(
+            Role.person_id == person_id and
+            Role.award_id == award_id
+        )
 
 
 class Role(UniqueMixin, Base):
     person_id = Column(
-        Integer, ForeignKey('person.id', ondelete='CASCADE'),
-        primary_key=True)
+        Integer,
+        ForeignKey('person.id', ondelete='CASCADE'),
+        primary_key=True,
+    )
     award_id = Column(
-        Integer, ForeignKey('award.id', ondelete='CASCADE'),
-        primary_key=True)
+        Integer,
+        ForeignKey('award.id', ondelete='CASCADE'),
+        primary_key=True,
+    )
     role = Column(Enum('pi', 'copi', 'fpi', 'po'))
     start = Column(Date)
     end = Column(Date)
 
-    award = saorm.relationship('Award', uselist=False, single_parent=True)
+    award = saorm.relationship(
+        'Award',
+        uselist=False,
+        single_parent=True,
+    )
     person = saorm.relationship(
-        'Person', uselist=False, single_parent=True,
+        'Person',
+        uselist=False,
+        single_parent=True,
         backref=saorm.backref(
-            'roles', cascade='all, delete-orphan', passive_deletes=True)
+            'roles',
+            cascade='all, delete-orphan',
+            passive_deletes=True,
+        ),
     )
 
     def __init__(self, person, award, role, start, end):
@@ -381,37 +569,54 @@ class Role(UniqueMixin, Base):
 
     @classmethod
     def unique_filter(cls, query, person, award, *args, **kwargs):
-        return query.filter(Role.person_id == person.id and
-                            Role.award_id == award.id)
+        return query.filter(
+            Role.person_id == person.id and
+            Role.award_id == award.id
+        )
 
 
 class Affiliation(UniqueMixin, Base):
     person_id = Column(
-        Integer, ForeignKey('person.id', ondelete='CASCADE'),
-        primary_key=True)
+        Integer,
+        ForeignKey('person.id', ondelete='CASCADE'),
+        primary_key=True,
+    )
     institution_id = Column(
-        Integer, ForeignKey('institution.id', ondelete='CASCADE'),
-        primary_key=True)
+        Integer,
+        ForeignKey('institution.id', ondelete='CASCADE'),
+        primary_key=True,
+    )
     award_id = Column(
-        Integer, ForeignKey('award.id', ondelete='CASCADE'),
-        primary_key=True)
+        Integer,
+        ForeignKey('award.id', ondelete='CASCADE'),
+        primary_key=True,
+    )
 
     person = saorm.relationship(
         'Person',
         backref=saorm.backref(
-            'affiliations', cascade='all, delete-orphan', passive_deletes=True)
+            'affiliations',
+            cascade='all, delete-orphan',
+            passive_deletes=True,
+        ),
     )
 
     institution = saorm.relationship(
         'Institution',
         backref=saorm.backref(
-            'affiliations', cascade='all, delete-orphan', passive_deletes=True)
+            'affiliations',
+            cascade='all, delete-orphan',
+            passive_deletes=True,
+        )
     )
 
     award = saorm.relationship(
         'Award',
         backref=saorm.backref(
-            'affiliations', cascade='all, delete-orphan', passive_deletes=True)
+            'affiliations',
+            cascade='all, delete-orphan',
+            passive_deletes=True,
+        ),
     )
 
     def __init__(self, person, institution, award):
